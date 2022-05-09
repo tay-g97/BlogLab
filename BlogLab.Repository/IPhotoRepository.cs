620370a4f9d4fe58435ca577dcd8c769170ed10a
tay-g97 <taylorgeorgeuk@yahoo.co.uk>
2022-05-09 13:57:19 +0100

    Completed repositories

---
+++ BlogLab.Repository/IPhotoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BlogLab.Models.Photo;
+
+namespace BlogLab.Repository
+{
+    public interface IPhotoRepository
+    {
+        public Task<Photo> InsertAsync(PhotoCreate photoCreate, int applicationUserId);
+        public Task<Photo> GetAsync(int photoId);
+        public Task<List<Photo>> GetAllByUserIdAsync(int applicationUserId);
+        public Task<int> DeleteAsync(int PhotoId);
+    }
+}
