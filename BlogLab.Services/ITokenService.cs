496b7ed77b94d2de58d23d88b30da0bfde967d86
tay-g97 <taylorgeorgeuk@yahoo.co.uk>
2022-05-09 15:06:16 +0100

    services project completed

---
+++ BlogLab.Services/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BlogLab.Models.Account;
+
+namespace BlogLab.Services
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUserIdentity user);
+    }
+}
